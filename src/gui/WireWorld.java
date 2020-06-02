7cce537adf92762ee56bc96e11afefa56a85b5d4
folejn <folejn@gmail.com>
2020-06-02 18:53:46 +0200

    poprawka do argumentów

--- src/gui/WireWorld.java
+++ src/gui/WireWorld.java
@@ -11,11 +11,16 @@ public class WireWorld {
     public static void main(String[] args) {
         int st = 0;
         gen = null;
-        File in = new File(args.length > 1 ? args[1] : "resources/inStruct.txt");
-        int steps = args.length > 2 ? Integer.parseInt(args[2]) : 20;
-        File out = new File(args.length >3 ? args[3] : "resources/out.txt");
+        File in = new File(args.length > 1 ? args[0] : "resources/inStruct.txt");
+        int steps = 0;
+        try {
+            steps = args.length > 2 ? Integer.parseInt(args[1]) : 20;
+        } catch(Exception e) {
+            steps = 20;
+        }
+        File out = new File(args.length >3 ? args[2] : "resources/out.txt");
         if(args.length > 4) {
-            outf = new String[args.length-4];
+            outf = new String[args.length-3];
             System.arraycopy(args,4,outf,0,outf.length);
             System.out.println(outf[0]);
         }
@@ -33,10 +38,11 @@ public class WireWorld {
             System.exit(1);
         }
         Gener finalGen = gen;
+        final int stepsN = steps;
         EventQueue.invokeLater(new Runnable() {
             @Override
             public void run() {
-                new SymulationWindow(finalGen,steps,out);
+                new SymulationWindow(finalGen,stepsN,out);
             }
         });
     }
